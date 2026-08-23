86b133f3393d1c130d4544ff74128f0f42fd56ef
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-23 11:08:27 +0800

    feat: Implement CSV file reading in CsvReader and initialize user_upload script to handle file input

--- bin/user_upload.php
+++ bin/user_upload.php
@@ -3,8 +3,20 @@
 
 declare(strict_types=1);
 
+use App\Csv\CsvReader;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 // TODO: parse $argv for --file <filename>, --dry-run, --create-table, --help
+if (in_array("--file", $argv)) {
+  $fileArgIndex = array_search('--file', $argv);
+  $file = $argv[$fileArgIndex + 1] ?? null;
+
+  $reader = new CsvReader();
+
+  if ($file) {
+    $reader->read($file);
+  }
+}
 // TODO: call App\Import\ImportService with the parsed options
 // TODO: print a report to STDOUT (should mirror the web UI's preview format)
--- src/Csv/CsvReader.php
+++ src/Csv/CsvReader.php
@@ -6,5 +6,16 @@ namespace App\Csv;
 
 class CsvReader
 {
+    public function read($file): void 
+    {
+
     // TODO
+    $handle = fopen($file, 'r');
+    
+    while (($row = fgetcsv($handle)) !== false) {
+        print_r($row);
+    }
+    
+        fclose($handle);
+    }
 }
