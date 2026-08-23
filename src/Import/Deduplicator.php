638480eb98f1d6bc55a133d222ad58b7c96d158b
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-23 13:39:11 +0800

    feat: implement Deduplicator class for email duplication checking

--- src/Import/Deduplicator.php
+++ src/Import/Deduplicator.php
@@ -6,5 +6,23 @@ namespace App\Import;
 
 class Deduplicator
 {
-    // TODO
+    private array $seenEmails = [];
+
+    public function isDuplicate(string $email): bool
+    {
+        $normalizedEmail = strtolower(trim($email));
+
+        if (in_array($normalizedEmail, $this->seenEmails)) {
+            return true;
+        } else {
+            $this->seenEmails[] = $normalizedEmail;
+            return false;
+        }
+    }
+
+  
+    public function reset(): void
+    {
+        $this->seenEmails = [];
+    }
 }
