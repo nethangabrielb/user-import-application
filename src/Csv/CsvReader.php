205fe57d6e01e344b8cd70289aa67167d4a97038
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-23 14:09:53 +0800

    refactor: update CsvReader to return array of rows and improve file readability check

--- src/Csv/CsvReader.php
+++ src/Csv/CsvReader.php
@@ -6,16 +6,29 @@ namespace App\Csv;
 
 class CsvReader
 {
-    public function read($file): void 
+    public function read(string $filePath): array 
     {
+        if (!is_readable($filePath)) {
+            throw new \RuntimeException("File at {$filePath} does not exist/not readable.");
+        } 
+        
+        $rows = [];
 
-    // TODO
-    $handle = fopen($file, 'r');
-    
-    while (($row = fgetcsv($handle)) !== false) {
-        print_r($row);
-    }
-    
+        $handle = fopen($filePath, 'r');
+
+        // skip the header row
+        $headers = fgetcsv($handle);
+
+        while (($row = fgetcsv($handle)) !== false) {
+            if (!$row) {
+                continue;
+            } else {
+                $rows[] = $row;
+            }
+        }
+
         fclose($handle);
+
+        return $rows;
     }
 }
